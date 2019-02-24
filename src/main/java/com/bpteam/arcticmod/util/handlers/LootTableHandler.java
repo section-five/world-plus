79b4209c26084354c3d390bc37c79cce79e767eb
Josia50 <elwyn.vdb@gmail.com>
2019-02-24 17:18:11 +0100

    ArcticMod + Event Handler

--- src/main/java/com/bpteam/arcticmod/util/handlers/LootTableHandler.java
+++ src/main/java/com/bpteam/arcticmod/util/handlers/LootTableHandler.java
@@ -1,10 +1,11 @@
 package com.bpteam.arcticmod.util.handlers;
 
+import com.bpteam.arcticmod.ArcticMod;
 import com.bpteam.arcticmod.Reference;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootTableList;
 
 public class LootTableHandler
 {
-    public static final ResourceLocation PENGUIN = LootTableList.register(new ResourceLocation(Reference.MODID, "penguin"));
+    public static final ResourceLocation PENGUIN = LootTableList.register(new ResourceLocation(ArcticMod.MODID, "penguin"));
 }
