1dc6c42a4c6c8d6662379d8f8458367d48e019e6
Josia50 <elwyn.vdb@gmail.com>
2019-03-01 19:58:07 +0100

    Fish!

--- src/main/java/com/bpteam/arcticmod/common/entities/EntityPenguin.java
+++ src/main/java/com/bpteam/arcticmod/common/entities/EntityPenguin.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
@@ -24,7 +25,7 @@ public class EntityPenguin extends EntityCow
         this.tasks.addTask(0, new EntityAISwimming(this));
         this.tasks.addTask(1, new EntityAIPanic(this, 1.5D));
         this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
-        this.tasks.addTask(3, new EntityAITempt(this, 1.25D, ModItems.WEAKER_ICE_BALL, false));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.25D, Items.FISH, false));
         this.tasks.addTask(4, new EntityAIFollowParent(this, 1.25D));
         this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 1.0D));
         this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
@@ -44,6 +45,8 @@ public class EntityPenguin extends EntityCow
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(40.0D);
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
     }
+
+
 
 
     @Override
