d6ecca6fbcc48584138e359012764434f38d73d8
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-03-06 20:15:54 -0600

    added penguin and seal meat

--- src/main/java/com/bpteam/arcticmod/init/ModItems.java
+++ src/main/java/com/bpteam/arcticmod/init/ModItems.java
@@ -12,6 +12,12 @@ public class ModItems {
     public static final Item WEAKER_ICE_BALL = new ItemWeakIceBall("weaker_ice_ball");
     public static final Item STRONGER_ICE_BALL = new ItemStrongIceBall("stronger_ice_ball");
     public static final Item HAMMER = new ItemBase("hammer");
+
+    //Foods
     public static final Item RAW_POLAR_BEAR = new FoodBase("raw_polar_bear", 2, 2F, true);
     public static final Item COOKED_POLAR_BEAR = new FoodBase("cooked_polar_bear", 4, 8F, true);
+    public static final Item RAW_PENGUIN = new FoodBase("raw_penguin", 2, 2F, true);
+    public static final Item COOKED_PENGUIN = new FoodBase("cooked_penguin", 4, 8F, true);
+    public static final Item RAW_SEAL = new FoodBase("raw_seal", 2, 2F, true);
+    public static final Item COOKED_SEAL = new FoodBase("cooked_seal", 4, 8F, true);
 }
