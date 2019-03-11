136b3e48890c229d9a1437d6826a822cdd98c238
Josia50 <elwyn.vdb@gmail.com>
2019-03-11 19:34:45 +0100

    Walrus

--- src/main/java/com/bpteam/arcticmod/common/entities/EntityStrongIceBall.java
+++ src/main/java/com/bpteam/arcticmod/common/entities/EntityStrongIceBall.java
@@ -11,63 +11,45 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class EntityStrongIceBall extends EntityThrowable
-{
-    public EntityStrongIceBall(World worldIn)
-    {
+public class EntityStrongIceBall extends EntityThrowable {
+    public EntityStrongIceBall(World worldIn) {
         super(worldIn);
     }
 
-    public EntityStrongIceBall(World worldIn, EntityLivingBase throwerIn)
-    {
+    public EntityStrongIceBall(World worldIn, EntityLivingBase throwerIn) {
         super(worldIn, throwerIn);
     }
 
-    public EntityStrongIceBall(World worldIn, double x, double y, double z)
-    {
+    public EntityStrongIceBall(World worldIn, double x, double y, double z) {
         super(worldIn, x, y, z);
     }
 
-    public static void registerFixesSnowball(DataFixer fixer)
-    {
+    public static void registerFixesSnowball(DataFixer fixer) {
         EntityThrowable.registerFixesThrowable(fixer, "StrongIceBall");
     }
 
-    /**
-     * Handler for {@link World#setEntityState}
-     */
     @SideOnly(Side.CLIENT)
-    public void handleStatusUpdate(byte id)
-    {
-        if (id == 3)
-        {
-            for (int i = 0; i < 8; ++i)
-            {
+    public void handleStatusUpdate(byte id) {
+        if (id == 3) {
+            for (int i = 0; i < 8; ++i) {
                 this.world.spawnParticle(EnumParticleTypes.SNOWBALL, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
             }
         }
     }
 
-    /**
-     * Called when this EntityThrowable hits a block or entity.
-     */
-    protected void onImpact(RayTraceResult result)
-    {
-        if (result.entityHit != null)
-        {
+    protected void onImpact(RayTraceResult result) {
+        if (result.entityHit != null) {
             int i = 0;
 
-            if (result.entityHit instanceof EntityBlaze)
-            {
+            if (result.entityHit instanceof EntityBlaze) {
                 i = 3;
             }
 
-            result.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float)i);
+            result.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float) i);
         }
 
-        if (!this.world.isRemote)
-        {
-            this.world.setEntityState(this, (byte)3);
+        if (!this.world.isRemote) {
+            this.world.setEntityState(this, (byte) 3);
             this.setDead();
         }
     }
--- src/main/java/com/bpteam/arcticmod/common/entities/EntityWeakIceBall.java
+++ src/main/java/com/bpteam/arcticmod/common/entities/EntityWeakIceBall.java
@@ -11,64 +11,46 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-    public class EntityWeakIceBall extends EntityThrowable
-    {
-        public EntityWeakIceBall(World worldIn)
-        {
-            super(worldIn);
-        }
+public class EntityWeakIceBall extends EntityThrowable {
+    public EntityWeakIceBall(World worldIn) {
+        super(worldIn);
+    }
 
-        public EntityWeakIceBall(World worldIn, EntityLivingBase throwerIn)
-        {
-            super(worldIn, throwerIn);
-        }
+    public EntityWeakIceBall(World worldIn, EntityLivingBase throwerIn) {
+        super(worldIn, throwerIn);
+    }
 
-        public EntityWeakIceBall(World worldIn, double x, double y, double z)
-        {
-            super(worldIn, x, y, z);
-        }
+    public EntityWeakIceBall(World worldIn, double x, double y, double z) {
+        super(worldIn, x, y, z);
+    }
 
-        public static void registerFixesSnowball(DataFixer fixer)
-        {
-            EntityThrowable.registerFixesThrowable(fixer, "WeakIceBall");
-        }
+    public static void registerFixesSnowball(DataFixer fixer) {
+        EntityThrowable.registerFixesThrowable(fixer, "WeakIceBall");
+    }
 
-        /**
-         * Handler for {@link World#setEntityState}
-         */
-        @SideOnly(Side.CLIENT)
-        public void handleStatusUpdate(byte id)
-        {
-            if (id == 3)
-            {
-                for (int i = 0; i < 8; ++i)
-                {
-                    this.world.spawnParticle(EnumParticleTypes.SNOWBALL, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
-                }
-            }
-        }
-
-        /**
-         * Called when this EntityThrowable hits a block or entity.
-         */
-        protected void onImpact(RayTraceResult result)
-        {
-            if (result.entityHit != null)
-            {
-                int i = 0;
-
-                if (result.entityHit instanceof EntityBlaze)
-                {
-                    i = 3;
-                }
-
-                result.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float)i);
-            }
-
-            if (!this.world.isRemote)
-            {
-                this.world.setEntityState(this, (byte)3);
-                this.setDead();
+    @SideOnly(Side.CLIENT)
+    public void handleStatusUpdate(byte id) {
+        if (id == 3) {
+            for (int i = 0; i < 8; ++i) {
+                this.world.spawnParticle(EnumParticleTypes.SNOWBALL, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
             }
         }
     }
+
+    protected void onImpact(RayTraceResult result) {
+        if (result.entityHit != null) {
+            int i = 0;
+
+            if (result.entityHit instanceof EntityBlaze) {
+                i = 3;
+            }
+
+            result.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float) i);
+        }
+
+        if (!this.world.isRemote) {
+            this.world.setEntityState(this, (byte) 3);
+            this.setDead();
+        }
+    }
+}
