fd8540899929026f082efba0468836720ee71878
Josia <38124619+Josia50@users.noreply.github.com>
2019-03-19 16:10:23 +0100

    Storm Amount

--- src/main/java/com/bpteam/worldplus/common/world/Worldinformation.java
+++ src/main/java/com/bpteam/worldplus/common/world/Worldinformation.java
@@ -4,6 +4,7 @@ public class Worldinformation {
 
     public static boolean wind_state;
     public static int wind_level;
+    public static int storm_amount;
 
     public static void setWind_state(boolean wind) {
         wind_state = wind;
@@ -20,4 +21,13 @@ public class Worldinformation {
     public static boolean getWind_state() {
         return wind_state;
     }
+    
+    public static void setStorm_Amount(int stormamount) {
+         storm_amount = stormamount
+        }
+    
+    public static iny getStorm_Amount() {
+        return storm_amount
+    }
+     
 }
