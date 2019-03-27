dd171f626754d8a827a00a523d92535cd4587d45
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-03-27 13:12:28 -0500

    smelting items work now :D

--- src/main/java/com/bpteam/worldplus/init/ModRecipes.java
+++ src/main/java/com/bpteam/worldplus/init/ModRecipes.java
@@ -6,8 +6,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModRecipes {
 
     public static void init() {
-        GameRegistry.addSmelting(ModItems.RAW_PENGUIN, new ItemStack(ModItems.COOKED_PENGUIN, 1),5);
-        GameRegistry.addSmelting(ModItems.RAW_POLAR_BEAR, new ItemStack(ModItems.COOKED_POLAR_BEAR, 1),5);
-        GameRegistry.addSmelting(ModItems.RAW_SEAL, new ItemStack(ModItems.COOKED_SEAL, 1),5);
+        GameRegistry.addSmelting(ModItems.RAW_PENGUIN, new ItemStack(ModItems.COOKED_PENGUIN, 1), 1.5f);
+        GameRegistry.addSmelting(ModItems.RAW_SEAL, new ItemStack(ModItems.COOKED_SEAL, 1), 1.5f);
+        GameRegistry.addSmelting(ModItems.RAW_POLAR_BEAR, new ItemStack(ModItems.COOKED_POLAR_BEAR, 1), 1.5f);
     }
 }
