5dcf94b8a1128864c873d23c1290674d9666469d
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-03-27 13:29:13 -0500

    v1.4.1

--- src/main/java/com/bpteam/worldplus/WorldPlus.java
+++ src/main/java/com/bpteam/worldplus/WorldPlus.java
@@ -19,7 +19,7 @@ public class WorldPlus
 {
     public static final String MODID = "worldplus";
     public static final String NAME = "World Plus";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.4.1";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2768,)";
     public static final String UPDATEURL = "https://raw.githubusercontent.com/bpteammc/world-plus/master/update.json";
 
