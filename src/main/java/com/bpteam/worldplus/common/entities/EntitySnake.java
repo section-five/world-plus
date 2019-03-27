aa5e9bde7195e45d3ef9b8e3ab3642ce056eccee
Josia50 <elwyn.vdb@gmail.com>
2019-03-27 19:25:14 +0100

    Snake will now attack

--- src/main/java/com/bpteam/worldplus/common/entities/EntitySnake.java
+++ src/main/java/com/bpteam/worldplus/common/entities/EntitySnake.java
@@ -1,28 +1,20 @@
 package com.bpteam.worldplus.common.entities;
 
-import com.bpteam.worldplus.util.handlers.LootTableHandler;
-import com.bpteam.worldplus.util.handlers.SoundsHandler;
-import ibxm.Player;
 import net.minecraft.block.Block;
-import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.*;
-import net.minecraft.entity.monster.EntitySlime;
-import net.minecraft.entity.monster.EntityVindicator;
-import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class EntitySnake extends EntityCow {
+public class EntitySnake extends EntityMob {
 
     public EntitySnake(World world) {
         super(world);
@@ -35,7 +27,9 @@ public class EntitySnake extends EntityCow {
         this.tasks.addTask(0, new EntityAISwimming(this));
         this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
         this.tasks.addTask(2, new EntityAILookIdle(this));
-        //this.tasks.addTask(3, new EntityAIAttackMelee (Player, 5F, false));
+        this.tasks.addTask(3, new EntityAIAttackMelee (this, 5F, false));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false, new Class[0]));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
     }
 
     @Override
@@ -48,6 +42,25 @@ public class EntitySnake extends EntityCow {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(40.0D);
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(2);
+    }
+
+    @Override
+    public boolean attackEntityAsMob(Entity entityIn)
+    {
+        if (!super.attackEntityAsMob(entityIn))
+        {
+            return false;
+        }
+        else
+        {
+            if (entityIn instanceof EntityLivingBase)
+            {
+                ((EntityLivingBase)entityIn).addPotionEffect(new PotionEffect(MobEffects.POISON, 200));
+            }
+
+            return true;
+        }
     }
 
     @Override
@@ -62,28 +75,29 @@ public class EntitySnake extends EntityCow {
 
     @Override
     public float getEyeHeight() {
-        return 0.6F;
+        return 0.2F;
     }
 
- //   @Override
-  //  protected ResourceLocation getLootTable() {
- //       return LootTableHandler.SNAKE;
- //   }
+   /* @Override
+    protected ResourceLocation getLootTable() {
+        return LootTableHandler.SNAKE;
+    }
 
-  //  @Override
-  //  protected SoundEvent getAmbientSound() {
-  //      return SoundsHandler.SNAKE_AMBIENT;
-//    }
+   @Override
+   protected SoundEvent getAmbientSound() {
+        return SoundsHandler.SNAKE_AMBIENT;
+   }
 
-//    @Override
- //   protected SoundEvent getHurtSound(DamageSource source) {
- //       return SoundsHandler.SNAKE_HURT;
- //   }
+   @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return SoundsHandler.SNAKE_HURT;
+    }
 
-   // @Override
-    //protected SoundEvent getDeathSound() {
-      //  return SoundsHandler.SNAKE_DEATH;
-    //}
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundsHandler.SNAKE_DEATH;
+    }*/
+
 
     @Override
     protected void playStepSound(BlockPos pos, Block blockIn) {
@@ -94,7 +108,7 @@ public class EntitySnake extends EntityCow {
         return getDataManager().get(TYPE);
     }
 
-    public void setType(int angelType) {
-        getDataManager().set(TYPE, angelType);
+    public void setType(int snaketype) {
+        getDataManager().set(TYPE, snaketype);
     }
 }
