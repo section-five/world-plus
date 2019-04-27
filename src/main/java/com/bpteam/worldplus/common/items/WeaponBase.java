2c49d207bc4b53ae3334e2e0c8944e8e875ab698
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-04-27 14:30:59 -0500

    added item luck cookie, and club

---
+++ src/main/java/com/bpteam/worldplus/common/items/WeaponBase.java
@@ -0,0 +1,22 @@
+package com.bpteam.worldplus.common.items;
+
+import com.bpteam.worldplus.WorldPlus;
+import com.bpteam.worldplus.init.ModItems;
+import com.bpteam.worldplus.util.IHasModel;
+import net.minecraft.item.ItemSword;
+
+public class WeaponBase extends ItemSword implements IHasModel {
+
+    public WeaponBase(String name, ToolMaterial material) {
+        super(material);
+        setTranslationKey(name);
+        setRegistryName(name);
+        setCreativeTab(WorldPlus.arctic);
+        ModItems.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels() {
+        WorldPlus.proxy.registerItemRenderer(this, 0 ,"inventory");
+    }
+}
